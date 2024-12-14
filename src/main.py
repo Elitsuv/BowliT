f86392d270e69795cb71836be25458787d062b43
Elitsuv <work.for.jeez999@gmail.com>
2024-12-14 15:52:03 +0530

    Fixed minor bugs in toss functions - [Changes 0.3]

--- src/main.py
+++ src/main.py
@@ -3,38 +3,50 @@ import random
 def toss():
     print("Welcome to Bowl-it. Would you like to toss? (Y/N): ")
     response = input("Please enter: ").capitalize()
-    if response == "Y":
+
+    if response == "Y" or "Yes":
         print("Let's toss!")
         
         user_choice = input("Choose Heads or Tails: ").capitalize()
+        while user_choice not in ["Heads", "Tails"]:
+            print("Invalid input. Please enter 'Heads' or 'Tails'.")
+            user_choice = input("Choose Heads or Tails: ").capitalize()
 
-        while True:
-            try:
-                user_number = int(input("Choose a number between 1 and 6: "))
-                if 1 <= user_number <= 6:
-                    break
-                else:
-                    print("Please enter a number between 1 and 6.")
-            except ValueError:
-                print("Invalid input. Enter a valid number between 1 and 6.")
-
+        user_number = int(input("Choose a number between 1 and 6: "))
         robot_number = random.randint(1, 6)
-        print(f"Robot chose: {robot_number}")
-
+        
         total = user_number + robot_number
-        toss_result = "Heads" if total % 2 != 0 else "Tails"
+        results = "Heads" if total % 2 != 0 else "Tails"
         print(f"The sum of numbers is: {total} ({'Odd' if total % 2 != 0 else 'Even'})")
-        print(f"The toss result is: {toss_result}")
+        print(f"The toss result is: {results}")
 
-        if user_choice == toss_result:
+        if user_choice == results:
             print("You win the toss!")
             return "user"
         else:
             print("Robot wins the toss!")
             return "robot"
+    elif response == "N" or "no":
+        print("Robot will toss.")
+        robot_choice = random.choice(["Heads", "Tails"])
+        print(f"Robot chose {robot_choice}")
+
+        robot_number = random.randint(1, 6)
+        user_number = int(input("Choose a number between 1 and 6: "))
+        
+        total = user_number + robot_number
+        results = "Heads" if total % 2 != 0 else "Tails"
+        print(f"The sum of numbers is: {total} ({'Odd' if total % 2 != 0 else 'Even'})")
+        print(f"The toss result is: {results}")
+
+        if robot_choice == results:
+            print("Robot wins the toss!")
+            return "robot"
+        else:
+            print("You win the toss!")
+            return "user"
     else:
-        print(":(")
-        return None
+        print("Invalid input. Please enter 'Y' or 'N'.")
 
 def batting_innings(player, opponent):
     score = 0
